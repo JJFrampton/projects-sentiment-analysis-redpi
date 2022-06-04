5f824898593ee3c6f129e3a00403bfa8c98d2328
joseph frampton <josephjamesframpton@gmail.com>
2022-06-04 14:16:33 -0600

    updated 2 level pi chart with lables
    - pi chart now has scores [center] and labels [outer]
    
    JJF

--- src/components/search.js
+++ src/components/search.js
@@ -30,6 +30,7 @@ function Search() {
   const [ error, setError ] = useState(null)
   const [ list, setList ] = useState([])
   const [ piData, setPiData ] = useState([])
+  const [ labelData, setLabelData ] = useState([])
 
   // const url = 'http://localhost:3001/api/comments/search/depp'
   let q = "depp"
@@ -44,11 +45,14 @@ function Search() {
         let neg = { name: "neg", value: 0 }
         let pos = { name: "pos", value: 0 }
         let neutral = { name: "neutral", value: 0 }
+        let labels = {}
         data.data.forEach((record) => {
-          console.log("RECORD : " + record)
           neg.value += record.neg
           pos.value += record.pos
           neutral.value += record.neutral
+
+          if (!labels[record.label]) { labels[record.label] = { name: record.label, value: 0 } }
+          labels[record.label].value += 1
         })
 
         // function clean(data, len) {
@@ -72,13 +76,18 @@ function Search() {
         neutral.value = Math.floor(neutral.value * 10)
 
         setPiData([ neg, pos, neutral ])
+        setLabelData(Object.values(labels))
       })
       .catch((e) => {
         console.log(e.message)
       })
   }, [])
   
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28'];
+  const COLORS = [
+    colors.red.hex,
+    colors.green.hex,
+    colors.yellow.hex
+  ]
 
   return (
     <div className="Search">
@@ -87,22 +96,27 @@ function Search() {
           {piData.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
-      </Pie>
+        </Pie>
+        <Pie data={labelData} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label >
+          {labelData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
       </PieChart>
       <AreaChart width={400} height={400} data={list}>
-        <Area type="monotone" dataKey="pos" stroke="#8884d8" fill="#8884d8" stackId="2" />
-        <Area type="monotone" dataKey="neutral" stroke="#ffc658" fill="#ffc658" stackId="2" />
-        <Area type="monotone" dataKey="neg" stroke="#82ca9d" fill="#82ca9d" stackId="2" />
+        <Area type="monotone" dataKey="pos" stroke={colors.green.hex} fill={colors.green.hex} stackId="2" />
+        <Area type="monotone" dataKey="neutral" stroke={colors.yellow.hex} fill={colors.yellow.hex} stackId="2" />
+        <Area type="monotone" dataKey="neg" stroke={colors.red.hex} fill={colors.red.hex} stackId="2" />
       </AreaChart>
       <AreaChart width={400} height={400} data={list} stackOffset="expand">
-        <Area type="monotone" dataKey="pos" stroke="#8884d8" fill="#8884d8" stackId="1" />
-        <Area type="monotone" dataKey="neg" stroke="#82ca9d" fill="#82ca9d" stackId="1" />
-        <Area type="monotone" dataKey="neutral" stroke="#ffc658" fill="#ffc658" stackId="1" />
+        <Area type="monotone" dataKey="pos" stroke={colors.green.hex} fill={colors.green.hex} stackId="1" />
+        <Area type="monotone" dataKey="neg" stroke={colors.red.hex} fill={colors.red.hex} stackId="1" />
+        <Area type="monotone" dataKey="neutral" stroke={colors.yellow.hex} fill={colors.yellow.hex} stackId="1" />
       </AreaChart>
       <LineChart width={400} height={400} data={list}>
-        <Line type="monotone" dataKey="pos" stroke="#8884d8" />
-        <Line type="monotone" dataKey="neg" stroke="#8774d8" />
-        <Line type="monotone" dataKey="neutral" stroke="#8774d8" />
+        <Line type="monotone" dataKey="pos" stroke={colors.green.hex} />
+        <Line type="monotone" dataKey="neg" stroke={colors.red.hex} />
+        <Line type="monotone" dataKey="neutral" stroke={colors.yellow.hex} />
       </LineChart>
     </div>
   );
