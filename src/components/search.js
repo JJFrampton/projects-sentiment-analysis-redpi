5305aa9760dbe7324dce49d9ce8e7226a9c238c5
Joseph Frampton <josephjamesframpton@gmail.com>
2022-06-11 14:22:46 -0600

    add responsive chart containers, flex box sizing with wrap for charts

--- src/components/search.js
+++ src/components/search.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState  } from 'react';
 import axios from 'axios';
-import { Tooltip, Cell, PieChart, Pie, AreaChart, Area, LineChart, Line  } from 'recharts';
+import { ResponsiveContainer, Tooltip, Cell, PieChart, Pie, AreaChart, Area, LineChart, Line  } from 'recharts';
 import colors from '../utils/colors'
 import { getSearch, parseStats } from '../connectors/api'
 
@@ -32,40 +32,69 @@ function Search() {
     neutral: colors.yellow.hex
   }
 
+  const lineChartMargins = { top: 0, right: 30, left: 30, bottom: 30 }
+
   return (
-    <div className="Search">
+    <div className="Search" style={{width: "100%"}}>
       <form onSubmit={handleSubmit}>
         <input id="searchBox" type="text" placeholder="Search.." value={searchTerm} onChange={e => setSearchTerm(e.target.value)}/>
         <input type="submit" hidden />
       </form>
-      <PieChart width={400} height={400}>
-        <Pie data={piData} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" >
-          {piData.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[entry.name]} />
-          ))}
-        </Pie>
-        <Pie data={labelData} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label >
-          {labelData.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[entry.name]} />
-          ))}
-        </Pie>
-        <Tooltip />
-      </PieChart>
-      <AreaChart width={400} height={400} data={list}>
-        <Area type="monotone" dataKey="pos" stroke={colors.green.hex} fill={colors.green.hex} stackId="2" />
-        <Area type="monotone" dataKey="neutral" stroke={colors.yellow.hex} fill={colors.yellow.hex} stackId="2" />
-        <Area type="monotone" dataKey="neg" stroke={colors.red.hex} fill={colors.red.hex} stackId="2" />
-      </AreaChart>
-      <AreaChart width={400} height={400} data={list} stackOffset="expand">
-        <Area type="monotone" dataKey="pos" stroke={colors.green.hex} fill={colors.green.hex} stackId="1" />
-        <Area type="monotone" dataKey="neg" stroke={colors.red.hex} fill={colors.red.hex} stackId="1" />
-        <Area type="monotone" dataKey="neutral" stroke={colors.yellow.hex} fill={colors.yellow.hex} stackId="1" />
-      </AreaChart>
-      <LineChart width={400} height={400} data={list}>
-        <Line type="monotone" dataKey="pos" stroke={colors.green.hex} />
-        <Line type="monotone" dataKey="neg" stroke={colors.red.hex} />
-        <Line type="monotone" dataKey="neutral" stroke={colors.yellow.hex} />
-      </LineChart>
+      <div className="charts" style={{width: '100%', display: "flex", flexWrap: "wrap"}}>
+        <ResponsiveContainer width="50%" height={400}
+        >
+          <PieChart>
+            <Pie data={piData} dataKey="value" cx="50%" cy="50%" outerRadius="60%" fill="#8884d8" >
+              {piData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[entry.name]} />
+              ))}
+            </Pie>
+            <Pie data={labelData} dataKey="value" cx="50%" cy="50%" innerRadius="65%" outerRadius="80%" fill="#82ca9d" label >
+              {labelData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[entry.name]} />
+              ))}
+            </Pie>
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+        <ResponsiveContainer
+          width="50%"
+          height={400}
+        >
+          <AreaChart
+            data={list}
+            margin={lineChartMargins}
+          >
+            <Area type="monotone" dataKey="pos" stroke={colors.green.hex} fill={colors.green.hex} stackId="2" />
+            <Area type="monotone" dataKey="neutral" stroke={colors.yellow.hex} fill={colors.yellow.hex} stackId="2" />
+            <Area type="monotone" dataKey="neg" stroke={colors.red.hex} fill={colors.red.hex} stackId="2" />
+          </AreaChart>
+        </ResponsiveContainer>
+        <ResponsiveContainer
+          width="100%"
+          height={400}
+        >
+          <AreaChart data={list} stackOffset="expand"
+            margin={lineChartMargins}
+          >
+            <Area type="monotone" dataKey="pos" stroke={colors.green.hex} fill={colors.green.hex} stackId="1" />
+            <Area type="monotone" dataKey="neg" stroke={colors.red.hex} fill={colors.red.hex} stackId="1" />
+            <Area type="monotone" dataKey="neutral" stroke={colors.yellow.hex} fill={colors.yellow.hex} stackId="1" />
+          </AreaChart>
+        </ResponsiveContainer>
+        <ResponsiveContainer
+          width="100%"
+          height={400}
+        >
+          <LineChart data={list}
+            margin={lineChartMargins}
+          >
+            <Line type="monotone" dataKey="pos" stroke={colors.green.hex} />
+            <Line type="monotone" dataKey="neg" stroke={colors.red.hex} />
+            <Line type="monotone" dataKey="neutral" stroke={colors.yellow.hex} />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
